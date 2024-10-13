6eacb05a75f026ae09cf2da8bf98e75491387305
MotieeAtefe <motiee1377@gmaile.com>
2024-10-13 21:11:09 +0330

    create AddStudent method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,32 @@
-﻿namespace LINQWithhColloction
+﻿using System.Collections.Generic;
+namespace LINQWithhColloction
 {
     internal class Program
     {
+        static List<Student> student = new List<Student>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+        static void AddStudent()
+        {
+            Console.Write("Id:");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Age:");
+            int Age = int.Parse(Console.ReadLine());
+            Console.Write("Department: ");
+            string department = Console.ReadLine();
+            Student newStudent = new Student()
+            {
+                Name = name,
+                Id = id,
+                Age = Age,
+                Department = department
+            };
+            student.Add(newStudent);
+        }
+            
     }
 }
