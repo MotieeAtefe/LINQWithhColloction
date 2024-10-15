a4813e887c518229fdfbf419a0c3b69ab0e32271
MotieeAtefe <motiee1377@gmaile.com>
2024-10-15 13:23:53 +0330

    create getting input user

--- Program.cs
+++ Program.cs
@@ -3,9 +3,30 @@ namespace LINQWithhColloction
 {
     internal class Program
     {
-        static List<Student> student = new List<Student>();
+        static List<Student> students = new List<Student>();
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Univercity Student Manager");
+                Console.WriteLine("1. Add Student");
+                Console.WriteLine("2. Display All Students");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter Your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        AddStudent();
+                        break;
+                    case 2:
+                        DisplayAllStudent();
+                        break;
+
+
+                }
+            }
             
         }
         static void AddStudent()
@@ -25,7 +46,27 @@ namespace LINQWithhColloction
                 Age = Age,
                 Department = department
             };
-            student.Add(newStudent);
+            students.Add(newStudent);
+            Console.WriteLine("Student added successfully!\n");
+
+            static void DisplayAllStudent()
+            {
+                if (students.Any())
+                {
+                    Console.WriteLine("\n show all Student");
+                    var studentQuery = from student in students select student;
+                    foreach (var student in studentQuery)
+                    {
+                        Console.WriteLine($"Id: {student.Id}\n,Name: {student.Name}\n,Age: {student.Age}\n, ");
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("\n not found ");
+                }
+            }
+
         }
             
     }
